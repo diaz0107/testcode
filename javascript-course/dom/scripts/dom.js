d2fe2acacde24ea9d1a170cd3fa87207ccb89eaa
Memin Diaz <104868541+Memin0107@users.noreply.github.com>
2022-12-09 09:44:20 -0500

    dom.js

--- javascript-course/dom/scripts/dom.js
+++ javascript-course/dom/scripts/dom.js
@@ -1,33 +1,26 @@
-function test() {
-  
-  console.log(document.getElementById('blue'));
-  document.getElementsByClassName('blue button').addEventListener('click', change_blue);
-  console.log(blue);
-  
-  let red = document.getElementsByClassName("red button");
-  red.addEventListener("click", change_red);
-  
-  let yellow = document.getElementsByClassName("yellow button");
-  yellow.addEventListener("click", change_yellow);
-  
-  function change_red() {
-    document.body.style.backgroundColor = "red";
-  }
-  function change_blue() {
-    document.body.style.backgroundColor = "blue";
-  }
-  function change_yellow() {
-    document.body.style.backgroundColor = "yellow";
-  }
-  
-  let submit = document.getElementsByClassName("submit");
-  submit.addEventListener("click", add_food);
-  
-  function add_food() {
-    var item = document.getElementsByClassName("fav_food").value;
-  
-    document.getElementsByClassName("add_food").innerHTML = "<p>" + item + "</p>";
-  }
-}
-test();
+let blue = document.getElementById("blue");
+blue.addEventListener("click", change_blue);
 
+let red = document.getElementById("red");
+red.addEventListener("click", change_red);
+
+let yellow = document.getElementById("yellow");
+yellow.addEventListener("click", change_yellow);
+
+function change_red() {
+  document.body.style.backgroundColor = "red";
+}
+function change_blue() {
+  document.body.style.backgroundColor = "blue";
+}
+function change_yellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+let submit = document.getElementById("submit");
+submit.addEventListener("click", add_food);
+
+function add_food() {
+  var item = document.getElementById("fav_food").value;
+
+  document.getElementById("add_food").innerHTML = "<p>" + item + "</p>";
+}
