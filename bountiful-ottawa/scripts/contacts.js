8d9778cfca7e0333dc05f39db65837a4ebf0679f
Memin Diaz <104868541+Memin0107@users.noreply.github.com>
2022-12-12 17:23:41 -0500

    index.html

--- bountiful-ottawa/scripts/contacts.js
+++ bountiful-ottawa/scripts/contacts.js
@@ -16,7 +16,7 @@ async function renderUsers() {
         let htmlSegment = `<div class="user">
                              <img src="${user.profileURL}" >
                              <h2>${user.firstName} ${user.lastName}</h2>
-                             <div class="email"><a href="email:${user.email}">${user.email}</a></div>
+                             <div class="position"><a href="email:${position}">${position}</a></div>
                            </div>`;
         html += htmlSegment;
     });
